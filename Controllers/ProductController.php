ce2f00821036266a7b16d4784fd65080320eabb1
Mooncode29 <irene.ravelonarivo@gmail.com>
2017-01-24 12:34:14 +0100

    step 2 ok, affichage cloths ok

--- Controllers/ProductController.php
+++ Controllers/ProductController.php
@@ -2,7 +2,7 @@
 
 require_once __DIR__.'/../Models/DB.php';
 require_once __DIR__.'/../Models/Vegetable.php';
-// require_once __DIR__.'/../Models/Cloth.php';
+require_once __DIR__.'/../Models/Cloth.php';
 
 
 class ProductController {
@@ -12,10 +12,10 @@ class ProductController {
 		for ($i = 0; $i<$nbVegetables; $i++){
 			$products[]= new Vegetable($i);
 		}
-		// $nbCloths = (new DB('cloths'))->count();
-		// for ($i = 0; $i<$nbCloths; $i++){
-		// 	$products[]= new Cloth($i);
-		// }
+		$nbCloths = (new DB('cloths'))->count();
+		for ($i = 0; $i<$nbCloths; $i++){
+			$products[]= new Cloth($i);
+		}
 		require __DIR__.'/../Views/productTable.php';
 	}
 
--- data/products.php
+++ data/products.php
@@ -3,6 +3,7 @@ $date = date('d/m/Y', time());
 return [
 	['id'=>'1','name'=>'carottes','price'=>'5€','productorName'=>'France', 'harvestedAt'=>$date],
 	['id'=>'2','name'=>'poireaux','price'=>'3€','productorName'=>'Espagne', 'harvestedAt'=>$date],
+
 	['id'=>'3','name'=>'pantalon','price'=>'20€','brand'=>'zara'],
 	['id'=>'4','name'=>'jupe','price'=>'15€','brand'=>'desigual'],
 	['id'=>'5','name'=>'robe','price'=>'30€','brand'=>'kiabi']
